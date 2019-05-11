a423a34d9b63eceda33ab86307d1b3442994dddb
Alberto Fernandez <Albertofdzm@gmail.com>
2019-05-11 20:15:43 +0200

    Add things

---
+++ Assets/GameController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private PlayerManager playerManager;
+    private PlayerController[] players;
+
+    void Awake () {
+        playerManager = FindObjectOfType<PlayerManager>();
+    }
+    
+    void Start () {
+        players = playerManager.players;
+    }
+
+    void Update () {
+      
+    }
+}
---
+++ Assets/O2Tank.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class O2Tank : MonoBehaviour
+{
+
+    public float CurrentO2 {
+        get {
+            return currentO2;
+        }
+    }
+
+    [SerializeField]
+    private float initialO2 = 100;
+    private float currentO2;
+    private float startTime;
+
+    void Start () {
+        currentO2 = initialO2;
+    }
+
+    void FixedUpdate () {
+        float o2ToSubtract = Time.deltaTime;
+        currentO2 -= o2ToSubtract;
+    }
+}
