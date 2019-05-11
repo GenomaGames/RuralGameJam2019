a1ac6e8d2b7beda7f52e8bd38739385622ca395c
Matto Godoy <mattogodoy@gmail.com>
2019-05-11 22:21:17 +0200

    Added bubbles spawner

---
+++ Assets/BubbleSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject bubblePrefab;
+    [SerializeField]
+    private float randomTimeMin = 0.5f;
+    [SerializeField]
+    private float randomTimeMax = 5f;
+    private Collider2D collider2D;
+
+    void Awake(){
+        collider2D = GetComponent<Collider2D>();
+    }
+
+    void Start(){
+        Invoke("RandomBubble", randomTimeMin);
+    }
+ 
+    void RandomBubble(){
+        float randomTime = Random.Range(randomTimeMin, randomTimeMax);
+
+        SpawnBubble();
+
+        Invoke("RandomBubble", randomTime);
+    }
+
+    private void SpawnBubble(){
+        float origin = Random.Range(collider2D.bounds.min.x, collider2D.bounds.max.x);
+        Vector2 position = new Vector2(origin, collider2D.transform.position.y);
+        Instantiate(bubblePrefab, position, Quaternion.identity, transform);
+    }
+}
