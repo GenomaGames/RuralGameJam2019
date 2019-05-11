1d2a577d3c46b42a187aee3addf8ccda6c5b9e4f
Matto Godoy <mattogodoy@gmail.com>
2019-05-11 21:49:09 +0200

    Added bubble destroyer

---
+++ Assets/DestroyPowerUps.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyPowerUps : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other){
+        BubbleController bubble = other.GetComponent<BubbleController>();
+        if(bubble != null){
+            Destroy(bubble.gameObject);
+        }
+
+    }
+}
